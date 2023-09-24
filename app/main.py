54c14c69f817bd171c1bf34941bb1e2f96893815
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-09-24 19:56:35 +0300

    added SBooking

--- app/main.py
+++ app/main.py
@@ -10,44 +10,45 @@ app = FastAPI()
 app.include_router(router_bookings)
 
 
-class SHotel(BaseModel):
-    address: str
-    name: str
-    stars: int
+# class SHotel(BaseModel):
+#     address: str
+#     name: str
+#     stars: int
 
-class HotelsSearchArgs:
-    def __init__(self,
-        location: str,
-        date_from: date, 
-        date_to: date,
-        has_spa: Optional[bool] = None,
-        stars: Optional[int] = Query(None, ge=1, le=5)):
+# class HotelsSearchArgs:
+#     def __init__(self,
+#         location: str,
+#         date_from: date, 
+#         date_to: date,
+#         has_spa: Optional[bool] = None,
+#         stars: Optional[int] = Query(None, ge=1, le=5)):
         
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.has_spa = has_spa
-        self.stars = stars
+#         self.location = location
+#         self.date_from = date_from
+#         self.date_to = date_to
+#         self.has_spa = has_spa
+#         self.stars = starsfrom app.database import async_session_maker
 
 
-@app.get("/hotels")
-def get_hotels(
-    search_args: HotelsSearchArgs = Depends()
-) -> list[SHotel]:
-    hotels = [
-        {
-            "address": "ул. Гагарина, 1, Алтай",
-            "name": "Super Hotel",
-            "stars": 5
-        },
-    ]
-    return hotels
 
-class SBooking(BaseModel):
-    room_id: int
-    date_from: date
-    date_to: date
+# @app.get("/hotels")
+# def get_hotels(
+#     search_args: HotelsSearchArgs = Depends()
+# ) -> list[SHotel]:
+#     hotels = [
+#         {
+#             "address": "ул. Гагарина, 1, Алтай",
+#             "name": "Super Hotel",
+#             "stars": 5
+#         },
+#     ]
+#     return hotels
 
-@app.post("/bookings")
-def add_booking(booking: SBooking):
-    pass
+# class SBooking(BaseModel):
+#     room_id: int
+#     date_from: date
+#     date_to: date
+
+# @app.post("/bookings")
+# def add_booking(booking: SBooking):
+#     pass
